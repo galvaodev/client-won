228660a8bdf9db56b74c380728424b8952fa0823
Macbook <galvaopp@gmail.com>
2021-02-22 22:15:40 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Bolerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="minifest" href="manifest.json" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJsand Style Component"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
