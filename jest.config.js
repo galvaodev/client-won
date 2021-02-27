2c0c29de4e7627a26ea47595b9859cbd7a2f44d0
joaomodesto <macbook@MBP-de-Macbook.home>
2021-02-26 23:33:15 -0300

    Feature: first components logo

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', './next/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/utils/', '/styles/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)', '!src/**/stores.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }
